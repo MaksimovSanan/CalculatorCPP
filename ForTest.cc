f0ba14a0e910ceaa10a7de90fd86d39f6a28a1a8
Song Rashida <songrash@at-i7.msk.21-school.ru>
2023-08-17 23:22:01 +0300

    qwe

--- ForTest.cc
+++ ForTest.cc
@@ -1,11 +1,17 @@
-#include "model.h"
+#include "controller.h"
 #include <iostream>
 
 int main(void) {
-    model test;
-    test.UpdateFunc("-3+5");
-    test.PrintCalifornia();
-    std::cout << test.Calculate(1.0) << std::endl;
+    // model test;
+    // test.UpdateFunc("-3+5");
+    // test.PrintCalifornia();
+    // std::cout << test.Calculate(1.0) << std::endl;
+
+    Controller test;
+    std::string xxx = "-3+5";
+    std::cout << xxx << std::endl;
+    test.Calculate(xxx, 10000000);
+    std::cout << xxx << std::endl;
 
     return  0;
 }
--- controller.h
+++ controller.h
@@ -1,5 +1,18 @@
 #ifndef CONTROLLER_CONTROLLER_CONTROLLER_H
 #define CONTROLLER_CONTROLLER_CONTROLLER_H
 
+#include "model.h"
+
+class Controller {
+    public:
+        void Calculate(std::string &func, double x) {
+            my_model.UpdateFunc(func);
+            func = std::to_string(my_model.Calculate(x));
+        }
+
+    private:
+        model my_model;
+
+};
 
 #endif
